155419adce2d2f006a71344da81b98b52958668c
unknown <eniolaerivon@gmail.com>
2020-05-26 06:10:35 +0100

    commiting

--- app/src/main/java/com/medecard/user/medecard_doctor/IntroActivity.java
+++ app/src/main/java/com/medecard/user/medecard_doctor/IntroActivity.java
@@ -1,10 +1,14 @@
 package com.medecard.user.medecard_doctor;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -16,11 +20,25 @@ public class IntroActivity extends AppCompatActivity {
     private ViewPager screenPager;
     IntroVewPagerAdapter introVewPagerAdapter;
     TextView textView;
+    TabLayout tabIndicator;
+    Button btnNext;
+    int position = 0;
+    Button btnGetStarted;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // make app on full screen
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_intro);
+        // hide the action bar
+        getSupportActionBar().hide();
+
+        // ini views
+        tabIndicator = findViewById(R.id.tab_indicator);
+        btnNext = findViewById(R.id.btn_next);
+        btnGetStarted = findViewById(R.id.btn_getStarted);
 
         // fill list screen
         List<ScreenItem> mList = new ArrayList<>();
@@ -34,7 +52,7 @@ public class IntroActivity extends AppCompatActivity {
         introVewPagerAdapter = new IntroVewPagerAdapter(this, mList);
         screenPager.setAdapter(introVewPagerAdapter);
 
-        textView = (TextView) findViewById(R.id.skip_btn);
+        textView = (TextView) findViewById(R.id.next_btn);
         textView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -42,5 +60,32 @@ public class IntroActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+        // setup tablayout with viewpager
+        tabIndicator.setupWithViewPager(screenPager);
+
+        // next button click Listener
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                position = screenPager.getCurrentItem();
+                if (position < mList.size()) {
+                    position++;
+                    screenPager.setCurrentItem(position);
+                }
+
+                if (position == mList.size()-1) { // when we reach the last screen
+                    // TODO : show the GETSTARTED button and hide the indicator and the next button
+                    loadLastScreen();
+                }
+            }
+        });
+    }
+
+        //show the GETSTARTED button and hide the indicator and the next button
+    private void loadLastScreen() {
+        btnNext.setVisibility(View.INVISIBLE);
+        btnGetStarted.setVisibility(View.VISIBLE);
+        tabIndicator.setVisibility(View.INVISIBLE);
     }
 }
--- app/src/main/java/com/medecard/user/medecard_doctor/SplashScreenActivity.java
+++ app/src/main/java/com/medecard/user/medecard_doctor/SplashScreenActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
@@ -18,7 +20,10 @@ public class SplashScreenActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash_screen);
+        getSupportActionBar().hide();
 
         imv = (ImageView) findViewById(R.id.app_theme);
         tv = (TextView) findViewById(R.id.app_theme_text);
